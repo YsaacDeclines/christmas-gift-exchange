e8aa927c5ebe4a886fc7a00fa3930688a233222f
YsaacDeclines <119654626+YsaacDeclines@users.noreply.github.com>
2024-11-09 23:59:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 let cachedClient = null;
 
